7456e2ece00f4375ff67d38f9d566e417afb4663
Het Dagli <89297674+hetdagli234@users.noreply.github.com>
2022-09-19 13:08:25 +0530

    Adding comments

--- programs_py/crunchy_vs_smooth.py
+++ programs_py/crunchy_vs_smooth.py
@@ -1,29 +1,36 @@
 # crunchy_vs_smooth
 # Built with Seahorse v0.1.6
 
+
 from seahorse.prelude import *
 
+# This will be updated once we do build the project, your program id can be found at crunchy_vs_smooth/target/idl/[your_project_name].json
 declare_id("Fo9d34XdUczXdNt9jkqrQseyUQys9bmTDC31utY3zt5x")
 
 
-class VoteAccount(Account):
-    owner: Pubkey
-    crunchy: u64
-    smooth: u64
-    bump: u8
-
-
+# Here we define all our instructions, each of the method below as an RPC end point which can be invoked by clients.
 @instruction
 def init(owner: Signer, voter: Empty[VoteAccount], vote_account_bump: u8):
+    # As a new user connects, we create a new voter PDA account for him and intialize the account.
     init_voter = voter.init(payer=owner, seeds=["Voter"])
     init_voter.bump = vote_account_bump
 
 
+# To vote crunchy
 @instruction
 def vote_crunchy(vote: VoteAccount):
     vote.crunchy += 1
 
 
+# To vote smooth
 @instruction
 def vote_smooth(vote: VoteAccount):
     vote.smooth += 1
+
+
+# Here we define the account required to be passed on to the instructions above.
+class VoteAccount(Account):
+    owner: Pubkey
+    crunchy: u64
+    smooth: u64
+    bump: u8
