8b0ae2beba644a8af9d59a5ebb58c29882d80295
Het Dagli <89297674+hetdagli234@users.noreply.github.com>
2022-09-19 15:17:56 +0530

    Adding more comments

--- programs_py/crunchy_vs_smooth.py
+++ programs_py/crunchy_vs_smooth.py
@@ -4,7 +4,7 @@
 
 from seahorse.prelude import *
 
-# This will be updated once we do build the project, your program id can be found at crunchy_vs_smooth/target/idl/[your_project_name].json
+# This will be updated once the project is build, your program id can be found at crunchy_vs_smooth/target/idl/[your_project_name].json
 declare_id("Fo9d34XdUczXdNt9jkqrQseyUQys9bmTDC31utY3zt5x")
 
 
@@ -13,25 +13,31 @@ declare_id("Fo9d34XdUczXdNt9jkqrQseyUQys9bmTDC31utY3zt5x")
 def init(owner: Signer, voter: Empty[VoteAccount], vote_account_bump: u8):
     # As a new user connects, we create a new voter PDA account for him and intialize the account.
     init_voter = voter.init(payer=owner, seeds=["Voter"])
+    # Assign the owner or the Signer of the one initialize the accouunt to the user's newly created VoteAccount owner.
     init_voter.owner = owner.key()
+    # Assign the bump to the one initializing the accouunt to the user's newly created VoteAccount bump.
     init_voter.bump = vote_account_bump
 
 
 # To vote crunchy
 @instruction
 def vote_crunchy(owner: Signer, vote: VoteAccount):
+    # Check if the public key of the signer is the same as the owner in the vote account.
     assert owner.key() == vote.owner, "This is not your Vote account!"
+    # Increment the crunchy variable in the user's VoteAccount
     vote.crunchy += 1
 
 
 # To vote smooth
 @instruction
 def vote_smooth(owner: Signer, vote: VoteAccount):
+    # Check if the public key of the signer is the same as the owner in the vote account.
     assert owner.key() == vote.owner, "This is not your Vote account!"
+    # Increment the smooth variable in the user's VoteAccount
     vote.smooth += 1
 
 
-# Here we define the account required to be passed on to the instructions above.
+# Defining the account which will be stored on-chain for every unique wallet interacting with our program.
 class VoteAccount(Account):
     owner: Pubkey
     crunchy: u64
